b8dd46b515b939fa4deed515c12f353ae441e0fc
Gmodery <gmodery@verizon.net>
2026-06-29 11:16:15 -0400

    proper orbital correction

--- solar-viz/src/data/bodies.js
+++ solar-viz/src/data/bodies.js
@@ -5,11 +5,11 @@ const planets = [
         radius: 1,
         color: "#8d8d8d",
         orbit: {
-            a: 0.427,      // semi-major axis (AU)
+            a: 0.387,      // semi-major axis (AU)
             e: 0.2056,     // eccentricity
             i: 7.005,      // inclination (degrees)
-            omega: 77.457, // argument of perihelion (degrees)
-            Omega: 48.33, // longitude of ascending node (degrees)
+            omega: 29.124, // argument of perihelion (degrees)
+            Omega: 48.331, // longitude of ascending node (degrees)
         },
         axialTilt: 0,
         texturePath: "/textures/Mercury.jpg"
@@ -20,11 +20,11 @@ const planets = [
         radius: 1.5,
         color: "#e9c551",
         orbit: {
-            a: 0.726,
+            a: 0.723,
             e: 0.0067,
-            i: 3.394,      
-            omega: 131.602, 
-            Omega: 76.679, 
+            i: 3.394,
+            omega: 54.884,
+            Omega: 76.680,
         },
         axialTilt: 177.3,
         texturePath: "/textures/Venus.jpg"
@@ -35,11 +35,11 @@ const planets = [
         radius: 1.6,
         color: "#1191fa",
         orbit: {
-            a: 1,      
+            a: 1.000,
             e: 0.0167,
-            i: -0.000015,      
-            omega: 102.937, 
-            Omega: 0, 
+            i: 0.000,
+            omega: 102.937,
+            Omega: 0.000,
         },
         axialTilt: 23.4,
         texturePath: "/textures/Earth.jpg"
@@ -50,11 +50,11 @@ const planets = [
         radius: 1.2,
         color: "#ca1616",
         orbit: {
-            a: 1.585,
-            e: 0.0933,     
-            i: 1.8496,
-            omega: -23.943, 
-            Omega: 49.559, 
+            a: 1.524,
+            e: 0.0934,
+            i: 1.850,
+            omega: 286.502,
+            Omega: 49.558,
         },
         axialTilt: 25.2,
         texturePath: "/textures/Mars.jpg"
@@ -65,11 +65,11 @@ const planets = [
         radius: 4,
         color: "#ce9700",
         orbit: {
-            a: 5.55,      
-            e: 0.0483,
-            i: 1.3043,      
-            omega: 14.728, 
-            Omega: 100.474, 
+            a: 5.203,
+            e: 0.0489,
+            i: 1.303,
+            omega: 273.867,
+            Omega: 100.464,
         },
         axialTilt: 3.1,
         texturePath: "/textures/Jupiter.jpg"
@@ -80,11 +80,11 @@ const planets = [
         radius: 3.5,
         color: "#ddb812",
         orbit: {
-            a: 9.01,      
-            e: 0.0538,
-            i: 2.4859,      
-            omega: 92.598, 
-            Omega: 113.622, 
+            a: 9.537,
+            e: 0.0565,
+            i: 2.485,
+            omega: 339.392,
+            Omega: 113.665,
         },
         axialTilt: 26.7,
         texturePath: "/textures/Saturn.jpg"
@@ -95,11 +95,11 @@ const planets = [
         radius: 2.5,
         color: "#0083c0",
         orbit: {
-            a: 18.5891,      
-            e: 0.04725,
-            i: 0.77263,      
-            omega: 170.954, 
-            Omega: 74.0169, 
+            a: 19.191,
+            e: 0.0463,
+            i: 0.773,
+            omega: 99.000,
+            Omega: 74.006,
         },
         axialTilt: 97.8,
         texturePath: "/textures/Uranus.jpg"
@@ -110,11 +110,11 @@ const planets = [
         radius: 1.2,
         color: "#0d59cc",
         orbit: {
-            a: 29.64,      
-            e: 0.00859,
-            i: 1.77,      
-            omega: 44.964, 
-            Omega: 131.784, 
+            a: 30.069,
+            e: 0.0100,
+            i: 1.770,
+            omega: 276.340,
+            Omega: 131.784,
         },
         axialTilt: 28.3,
         texturePath: "/textures/Neptune.jpg"
